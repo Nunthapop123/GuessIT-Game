eb18d069b8f930a974af1c7a329041a03e7e17c8
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2025-04-10 12:57:31 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+import random
+
+
+class Letter:
+    pass
+
+
+class Word:
+    pass
+
+
+class Player:
+    pass
+
+
+class Item:
+    pass
+
+
+class Shop:
+    pass
+
+
+class GameManager:
+    pass
