99de43e5e574f4be868be1942bd44db183fb285c
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2025-04-12 08:53:15 +0700

    modify the code to base on GameManager class

--- app.py
+++ app.py
@@ -9,30 +9,14 @@ HEIGHT = 1024
 SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 BACKGROUND = pg.image.load("assets/Starting Tiles(white).png")
 BACKGROUND_RECT = BACKGROUND.get_rect(center=(640, 450))
-LETTER_FONT = pg.font.Font("assets/PressStart2P-vaV7.ttf", 40)
 
 OUTLINE = "#d3d6da"
 FILLED_OUTLINE = "#878a8c"
 
-HEADER = LETTER_FONT.render('GuessIT!', True, 'black')
-HEADER_RECT = HEADER.get_rect()
-HEADER_RECT.center = ((1280 // 2, 100))
-
-SCREEN.fill("white")
-SCREEN.blit(BACKGROUND, BACKGROUND_RECT)
-SCREEN.blit(HEADER, HEADER_RECT)
-pg.display.update()
-
+LETTER_FONT = pg.font.Font("assets/PressStart2P-vaV7.ttf", 40)
 LETTER_X_SPACING = 85
 LETTER_Y_SPACING = 162
 LETTER_SIZE = 75
-
-guesses_count = 0
-guesses = [[]] * 6
-
-current_guess = []
-current_guess_letter = ""
-current_letter_bg_x = 432
 
 
 class Letter:
@@ -62,42 +46,6 @@ class Letter:
         pg.display.update()
 
 
-def create_new_letter():
-    global current_guess_letter, current_letter_bg_x
-    current_guess_letter += key_pressed
-    new_letter = Letter(key_pressed, (current_letter_bg_x, guesses_count * 100 + LETTER_Y_SPACING))
-    current_letter_bg_x += LETTER_X_SPACING
-    guesses[guesses_count].append(new_letter)
-    current_guess.append(new_letter)
-    for guess in guesses:
-        for letter in guess:
-            letter.draw()
-
-
-def delete_letter():
-    global current_guess_letter, current_letter_bg_x
-    guesses[guesses_count][-1].delete()
-    guesses[guesses_count].pop()
-    current_guess_letter = current_guess_letter[:-1]
-    current_guess.pop()
-    current_letter_bg_x -= LETTER_X_SPACING
-
-
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-        if event.type == pg.KEYDOWN:
-            key_pressed = event.unicode.upper()
-            if key_pressed in "QWERTYUIOPASDFGHJKLZXCVBNM" and key_pressed != "":
-                if len(current_guess_letter) < 5:
-                    create_new_letter()
-            if event.key == pg.K_BACKSPACE:
-                if len(current_guess_letter) > 0:
-                    delete_letter()
-
-
 class Word:
     pass
 
@@ -115,4 +63,58 @@ class Shop:
 
 
 class GameManager:
-    pass
+    def __init__(self):
+        self.guesses = [[]] * 6
+        self.current_guess = []
+        self.current_guess_letter = ""
+        self.current_letter_bg_x = 432
+        self.guesses_count = 0
+        self.running = True
+        self.starter_display()
+
+    def starter_display(self):
+        SCREEN.fill("white")
+        SCREEN.blit(BACKGROUND, BACKGROUND_RECT)
+        header = LETTER_FONT.render("GuessIT!", True, "black")
+        header_rect = header.get_rect(center=(WIDTH // 2, 100))
+        SCREEN.blit(header, header_rect)
+        pg.display.update()
+
+    def create_new_letter(self, key_pressed):
+        self.current_guess_letter += key_pressed
+        new_letter = Letter(key_pressed, (self.current_letter_bg_x, self.guesses_count * 100 + LETTER_Y_SPACING))
+        self.current_letter_bg_x += LETTER_X_SPACING
+        self.guesses[self.guesses_count].append(new_letter)
+        self.current_guess.append(new_letter)
+        for guess in self.guesses:
+            for letter in guess:
+                letter.draw()
+
+    def delete_letter(self):
+        self.current_guess[-1].delete()
+        self.guesses[self.guesses_count].pop()
+        self.current_guess.pop()
+        self.current_guess_letter = self.current_guess_letter[:-1]
+        self.current_letter_bg_x -= LETTER_X_SPACING
+
+    def game_loop(self):
+        while self.running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+                    pg.quit()
+                    sys.exit()
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_BACKSPACE:
+                        if len(self.current_guess_letter) > 0:
+                            self.delete_letter()
+                    else:
+                        key_pressed = event.unicode.upper()
+                        if key_pressed in "QWERTYUIOPASDFGHJKLZXCVBNM" and key_pressed != "":
+                            if len(self.current_guess_letter) < 5:
+                                self.create_new_letter(key_pressed)
+
+
+if __name__ == '__main__':
+    game = GameManager()
+    game.game_loop()
